1362a579e907dedc169ef805df27d8d9965dd124
Swagato Bag <1805537@kiit.ac.in>
2022-04-10 11:51:32 +0530

    Updated Canvas Color

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
-var canvas,ctx;
+var canvas,ctx, canvasColor = "black";
+var lineWidth = 15, strokeColor = 'yellow';
 var mouseX,mouseY,isMouseDown = false;
 var touchX,touchY;
 var isResultDivPresent = false;
 let model;
-
-
 //the base url of website in which our web app is deployed is obtained from window.location.origin the json file is loaded using async function
 var base_url = window.location.origin;
 
@@ -12,24 +11,23 @@ var base_url = window.location.origin;
 /* Loading the Model */
 
 (async function(){  
-    // console.log("Model loading...");  
+    console.log("Model loading...");  
     // for local testing
-    // model = await tf.loadLayersModel("https://swagatobag2000.github.io/digit-recognition-webapp/models/model.json")
+    model = await tf.loadLayersModel("https://swagatobag2000.github.io/digit-recognition-webapp/models/model.json")
     // for deployment
-    model = await tf.loadLayersModel("../models/model.json");
-    // console.log("Model loaded..");
+    // model = await tf.loadLayersModel("../models/model.json");
+    console.log("Model loaded..");
 })();
 
 
 /* fucnction for interacting with canvas */
 
 function init(){
-    canvas = document.getElementById('sketchpad');
-
-    
+    canvas = document.getElementById('sketchpad');   
     ctx = canvas.getContext('2d'); // '2d' means two dimensional rendering context on canvas
-    ctx.fillStyle = "white"; // now we will fill ctx background with black
+    ctx.fillStyle = canvasColor; // now we will fill ctx background with black
     ctx.fillRect(0,0,canvas.width,canvas.height); // fill the rect
+    console.log("Canvas Width: ",canvas.width,canvas.height);
     if(ctx){
         //if mousedown than call function sketchPad_mouseDown.false means bubble phase
         canvas.addEventListener('mousedown', sketchpad_mouseDown, false);          
@@ -42,11 +40,11 @@ function init(){
 
 /* now to enable drawing on canvas we define draw function */
 
-function drawWithMouse(ctx,x,y,isDown){
+function drawWithMouse(ctx,x,y,size,isDown){
     if(isDown){   
         ctx.beginPath(); //to inform canvas, user is about to draw
-        ctx.strokeStyle = "red"; //to set background color of canvas
-        ctx.lineWidth = '20'; //set width of line      
+        ctx.strokeStyle = strokeColor; //to set background color of canvas
+        ctx.lineWidth = lineWidth; //set width of line      
         
         // .linejoin() : set connection between two line, 
         // .lineCap() : to set end of line         
@@ -65,26 +63,28 @@ function drawWithMouse(ctx,x,y,isDown){
 /*Event handlers*/
 
 /* when mouse is down it will call draw function */
-function sketchpad_mouseDown() {
+function sketchpad_mouseDown(event) {
     isMouseDown = true;    
-    drawWithMouse(ctx,mouseX,mouseY,false);
+    drawWithMouse(ctx,mouseX,mouseY,12,false);
 }
 
 /* when mouse is released it set's mousedown back to false and start prediction */
-async function sketchpad_mouseUp() {    
+async function sketchpad_mouseUp(event) {    
     isMouseDown = false;
+    var imageData = canvas.toDataURL();    
     let tensor = preprocessCanvas(canvas); 
+    console.log("Tensor after preprocessing: ", tensor)   
     let predictions = await model.predict(tensor).data();  
     let results = Array.from(predictions);    
-    displayLabel(results);    
+    displayLabel(results);   
 }
 
 /* when mouse is moved in either direction it gets current position of mouse from getMousePos(e) and if mouseDown than call draw */
 
-function sketchpad_mouseMove(e) {
-    getMousePos(e);
+function sketchpad_mouseMove(event) {
+    getMousePos(event);
     if (isMouseDown) {
-        drawWithMouse(ctx,mouseX,mouseY,true);
+        drawWithMouse(ctx,mouseX,mouseY,12,true);
     }
 }
 
@@ -92,16 +92,14 @@ function sketchpad_mouseMove(e) {
 when mouse event is triggered, offset x,offset y -> return x,y cordinate of mouse
 and layer x,layer y -> return horizantak and vertical cordinates relative to current layer */
 
-function getMousePos(e) {    
-    if (!e)        
-      var e = event;     
-    if (e.offsetX) {        
-      mouseX = e.offsetX;        
-      mouseY = e.offsetY;    
+function getMousePos(event) {       
+    if (event.offsetX) {        
+      mouseX = event.offsetX;        
+      mouseY = event.offsetY;    
     }    
-    else if (e.layerX) {        
-      mouseX = e.layerX;        
-      mouseY = e.layerY;    
+    else if (event.layerX) {        
+      mouseX = event.layerX;        
+      mouseY = event.layerY;    
     } 
 }
 
@@ -109,28 +107,26 @@ function getMousePos(e) {
 /* touch event handler */
 
 /* when user touches the touchpad it calls draw func with false to note position not to draw */
-function sketchpad_touchStart() {     
-    getTouchPos();    
-    drawWithMouse(ctx,touchX,touchY,false);    
+function sketchpad_touchStart(event) {     
+    getTouchPos(event);    
+    drawWithMouse(ctx,touchX,touchY,12,false);    
     event.preventDefault(); //this prevents scrolling of screen when user draws
 }
 
 /* when user drags in sketchpad it calls draw with true flag to enable drawing */
 
-function sketchpad_touchMove(e) {     
-    getTouchPos(e);    
-    drawWithMouse(ctx,touchX,touchY,true);    
+function sketchpad_touchMove(event) {     
+    getTouchPos(event);    
+    drawWithMouse(ctx,touchX,touchY,12,true);    
     event.preventDefault();
 }
 
 /* it is used to find point in the sketchpad where user has touched */
 
-function getTouchPos(e) {    
-    if (!e)        
-    var e = event;     
-    if(e.touches) {   
-      if (e.touches.length == 1) {  //its length is used to find  how many fingers has touched               
-        var touch = e.touches[0];            
+function getTouchPos(event) {        
+    if(event.touches) {   
+      if (event.touches.length == 1) {  //its length is used to find  how many fingers has touched               
+        var touch = event.touches[0];            
         touchX=touch.pageX-touch.target.offsetLeft;               
         touchY=touch.pageY-touch.target.offsetTop;        
       }
@@ -140,7 +136,7 @@ function getTouchPos(e) {
 /* clearing the sketchpad */
 function clear(){  
     ctx.clearRect(0, 0, canvas.width, canvas.height);  
-    ctx.fillStyle = "white"; 
+    ctx.fillStyle = canvasColor; 
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     clearAll();
     isResultDivPresent = false;
@@ -161,7 +157,6 @@ tensor.div() -> used to divide the array or tensor by the maximum RGB value(255)
 
 function preprocessCanvas(image) { 
     let tensor = tf.browser.fromPixels(image).resizeNearestNeighbor([28, 28]).mean(2).expandDims(2).expandDims().toFloat(); 
-    // console.log(tensor.shape); 
     return tensor.div(255.0);
 }
 
